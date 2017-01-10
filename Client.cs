c3d3377b4bdd8467f8d20f7d4308974d853500c5
AllForOwl <sahavrysyuk@gmail.com>
2017-01-10 15:13:29 +0400

    Update messages

--- Client.cs
+++ Client.cs
@@ -16,6 +16,7 @@ using WebSocket4Net;
 using System.Diagnostics;
 using Microsoft;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace Chat
 {
@@ -38,6 +39,30 @@ namespace Chat
         private string m_CertificateFile;
         private string m_Password;
 
+        private List<string> m_listUnreadMessages = new List<string>();
+
+        public bool isNewMessages() 
+        {
+            if (m_listUnreadMessages.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<string> GetNewMessages()
+        {
+            return m_listUnreadMessages;
+        }
+        public void ClearListMessages()
+        {
+            m_listUnreadMessages.Clear();
+            int a = 5;
+        }
+
         public void StartClient()
         {
             m_WebSocketClient.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(m_WebSocketClient_Error);
@@ -51,22 +76,6 @@ namespace Chat
             {
                 
             }
-
-            //for (var i = 0; i < 10; i++)
-            //{
-            //    var message = Guid.NewGuid().ToString();
-
-            //    m_WebSocketClient.Send(message);
-
-            //    if (!m_MessageReceiveEvent.WaitOne(5000))
-            //    {
-            //       // Assert.Fail("Failed to get echo messsage on time");
-            //        break;
-            //    }
-
-            //    Console.WriteLine("Received echo message: {0}", m_CurrentMessage);
-            //    //Assert.AreEqual(m_CurrentMessage, message);
-            //}
         }
 
         public void SendMessage(string i_message)
@@ -86,37 +95,14 @@ namespace Chat
 
         public void CloseWebSocketTest()
         {
-            //WebSocket m_WebSocketClient = new WebSocket(string.Format("{0}:{1}/websocket", Host, m_WebSocketClient.Config.Port), "basic", m_Version);
-            //m_WebSocketClient.AllowUnstrustedCertificate = true;
-            //m_WebSocketClient.Opened += new EventHandler(m_WebSocketClient_Opened);
-            //m_WebSocketClient.Closed += new EventHandler(m_WebSocketClient_Closed);
-            //m_WebSocketClient.MessageReceived += new EventHandler<MessageReceivedEventArgs>(m_WebSocketClient_MessageReceived);
-            //m_WebSocketClient.Open();
 
-            //if (!m_OpenedEvent.WaitOne(2000))
-            //{
- 
-            //}
-            //   // Assert.Fail("Failed to Opened session ontime");
-
-            ////Assert.AreEqual(WebSocketState.Open, m_WebSocketClient.State);
-
-            //m_WebSocketClient.Send("QUIT");
-
-            //if (!m_CloseEvent.WaitOne(1000))
-            //{
- 
-            //}//Assert.Fail("Failed to close session ontime");
-
-            //Assert.AreEqual(WebSocketState.Closed, m_WebSocketClient.State);
         }
 
         protected void m_WebSocketClient_MessageReceived(object sender, MessageReceivedEventArgs e)
         {
             m_CurrentMessage = e.Message;
             m_MessageReceiveEvent.Set();
-
-            MessageBox.Show(m_CurrentMessage);  
+            m_listUnreadMessages.Add(m_CurrentMessage);
         }
 
         protected void m_WebSocketClient_Closed(object sender, EventArgs e)
@@ -131,3 +117,8 @@ namespace Chat
 
     }
 }
+
+
+/*
+ * Add Database for save hitory Messages
+ */
--- FormClient.cs
+++ FormClient.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
 using Chat;
 
 namespace Chat
@@ -14,6 +15,7 @@ namespace Chat
     public partial class FormClient : Form
     {
         Client m_client = new Client();
+        System.Timers.Timer m_timer;
 
         public FormClient()
         {
@@ -23,14 +25,62 @@ namespace Chat
         private void FormClient_Load(object sender, EventArgs e)
         {
             m_client.StartClient();
+            SetTimer();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             m_client.SendMessage(ui_textMessage.Text);
-            ui_historyMessage.Items.Add(ui_textMessage.Text);
             ui_textMessage.Clear();
-
+        }
+        private void Update(Object source, ElapsedEventArgs e)
+        {
+            if (m_client.isNewMessages())
+            {
+                List<string> _list = new List<string>(m_client.GetNewMessages());
+                m_client.ClearListMessages();
+                for (int i = 0; i < _list.Count(); i++)
+                {
+                    ui_historyMessage.Items.Add(_list[i]);
+                }
+            }
+        }
+        private void SetTimer()
+        {            
+            m_timer = new System.Timers.Timer(1000);
+            
+            m_timer.Elapsed += Update;
+            m_timer.AutoReset = true;
+            m_timer.Enabled = true;
         }
     }
 }
+
+/*
+ * SetTimer();
+
+      Console.WriteLine("\nPress the Enter key to exit the application...\n");
+      Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+      Console.ReadLine();
+      aTimer.Stop();
+      aTimer.Dispose();
+
+      Console.WriteLine("Terminating the application...");
+   }
+
+   private static void SetTimer()
+   {
+        // Create a timer with a two second interval.
+        aTimer = new  System.Timers.Timer(2000);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                          e.SignalTime);
+    }
+ */
